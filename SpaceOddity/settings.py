2c84cc828e52bc02ceb86cb785bf6ba03458ce5c
Schoelsy <Schoelsy@wp.pl>
2017-11-02 21:38:22 +0100

    'fix4'

--- SpaceOddity/settings.py
+++ SpaceOddity/settings.py
@@ -9,9 +9,14 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
 from decouple import config
+import dj_database_url
+db_from_env=dj_database_url.config()
+
+
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
