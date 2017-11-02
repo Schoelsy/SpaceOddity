b8ee7da96ba05fa9508980ccb65bb54d3da73699
Schoelsy <Schoelsy@wp.pl>
2017-11-02 22:04:25 +0100

    'fix8'

--- SpaceOddity/settings.py
+++ SpaceOddity/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 import os
 from decouple import config
-import dj_database_url
-db_from_env=dj_database_url.config()
+from dj_database_url import parse as dburl
+
 
 
 
